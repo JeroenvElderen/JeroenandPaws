ca26b4235893a246194e00a1e4726df53d9eb0cd
Jeroen van Elderen <109440351+JeroenvElderen@users.noreply.github.com>
2025-09-30 15:36:58 +0100

    Convert Webflow home page into JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
 import { useEffect } from 'react';
 import './styles/normalize.css';
 import './styles/webflow.css';
 import './styles/jeroen-paws.webflow.css';
-import { originalLayoutHtml } from './originalMarkup.js';
+import Home from './pages/Home.jsx';
 
 function App() {
   useEffect(() => {
@@ -42,29 +41,7 @@ function App() {
     };
   }, []);
 
-  return <div dangerouslySetInnerHTML={{ __html: originalLayoutHtml }} />;
-import Header from './components/Header.jsx';
-import Hero from './components/Hero.jsx';
-import Services from './components/Services.jsx';
-import Highlights from './components/Highlights.jsx';
-import Showcase from './components/Showcase.jsx';
-import Testimonials from './components/Testimonials.jsx';
-import Footer from './components/Footer.jsx';
-
-function App() {
-  return (
-    <div id="top">
-      <Header />
-      <main>
-        <Hero />
-        <Highlights />
-        <Services />
-        <Showcase />
-        <Testimonials />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
