7e0e262bc4d90f3a159b7de1b2ea904362b7bfb3
JeroenvElderen <jeroen.vanelderen@hotmail.com>
2025-10-03 15:35:11 +0100

    update and added supabase tables schemes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import AboutHeroSection from './sections/AboutHeroSection';
 import AboutOverviewSection from './sections/AboutOverviewSection';
 import AboutGallerySection from './sections/AboutGallerySection';
 
 const About = () => (
   <main>
+    <AboutHeroSection />
     <AboutOverviewSection />
     <AboutGallerySection />
   </main>
