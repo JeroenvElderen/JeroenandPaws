c3feb4267d1ad6652766f4e4caa3e14b2a927b2c
JeroenSverige <77346416+JeroenSverige@users.noreply.github.com>
2026-01-21 16:45:41 +0000

    verify tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html"
   ],
   theme: {
     extend: {
