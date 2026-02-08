ec8375abe7e6032892c13047bcbe7ca7d90c3d50
JeroenSverige <77346416+JeroenSverige@users.noreply.github.com>
2026-02-08 20:41:22 +0000

    APi url

--- mobile-app/src/api/config.js
+++ mobile-app/src/api/config.js
@@ -1,7 +1,8 @@
 import Constants from "expo-constants";
 
 export const API_BASE_URL =
-  Constants.expoConfig?.extra?.apiBaseUrl ||
+Constants.expoConfig?.extra?.API_BASE_URL ||  
+Constants.expoConfig?.extra?.apiBaseUrl ||
   process.env.EXPO_PUBLIC_API_BASE_URL ||
   process.env.API_BASE_URL ||
   "https://jeroenandpaws.com";
